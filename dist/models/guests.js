a3b344ddecb6543ede559f9b73eaf1c6c40cc79f
jonathan-vickers-wttech <jonathan.vickers@wundermanthompson.com>
2024-04-25 14:11:58 +0100

    update file name to fix aws upload

--- dist/models/guests.js
+++ dist/models/guests.js
@@ -1,69 +1,74 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const slugify_1 = __importDefault(require("slugify"));
-const GuestSchema = new mongoose_1.default.Schema({
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const mongoose_1 = __importDefault(require('mongoose'));
+const slugify_1 = __importDefault(require('slugify'));
+const GuestSchema = new mongoose_1.default.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name cannot be more than 50 characters'],
+      type: String,
+      required: [true, 'Please add a name'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name cannot be more than 50 characters'],
     },
     email: {
-        type: String,
-        match: [
-            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
-            'Please add a valid email',
-        ],
+      type: String,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+        'Please add a valid email',
+      ],
     },
     slug: String,
     accomodation: {
-        type: String,
+      type: String,
     },
     attending: {
-        type: String,
+      type: String,
     },
     price: {
-        type: Number,
+      type: Number,
     },
     fullDay: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     notes: {
-        type: String,
-        maxlength: [500, 'Notes cannot be more than 500 characters'],
+      type: String,
+      maxlength: [500, 'Notes cannot be more than 500 characters'],
     },
     lunch: {
-        type: String,
+      type: String,
     },
     hotdog: {
-        type: String,
+      type: String,
     },
     dietryRequirements: {
-        type: String,
+      type: String,
     },
-}, {
+  },
+  {
     timestamps: true,
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-});
+  }
+);
 // create guest slug from the name
 GuestSchema.pre('save', function (next) {
-    this.slug = (0, slugify_1.default)(this.name, { lower: true });
-    next();
+  this.slug = (0, slugify_1.default)(this.name, { lower: true });
+  next();
 });
 GuestSchema.virtual('accomodationTents', {
-    ref: 'Accomodation',
-    localField: '_id',
-    foreignField: 'guests',
-    justOne: true,
+  ref: 'Accomodation',
+  localField: '_id',
+  foreignField: 'guests',
+  justOne: true,
 });
 GuestSchema.pre('find', function () {
-    this.populate('accomodationTents');
+  this.populate('accomodationTents');
 });
 exports.default = mongoose_1.default.model('Guest', GuestSchema);
